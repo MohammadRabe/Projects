d757ae8c7d1cbc0920d07c3b87b1094b8bd663b9
MohammadRabea <112767251+MohammadRabe@users.noreply.github.com>
2026-08-02 16:11:33 +0300

    Add files via upload

---
+++ professional-portfolio/src/components/hero-2/Hero_2.jsx
@@ -0,0 +1,40 @@
+
+const Hero_2 = () => {
+  return (
+    <div id="hero-2" className="hero hero-2 d-flex align-items-center">
+
+        <div className="d-flex flex-column flex-md-row gap-5 justify-content-center align-items-center ">
+            <h1 className="text-shadow text-dark align-self-md-start m-0 me-5">About Me</h1>
+            <div className="text-btn text-start">
+                <p className="text-start">Lorem ipsum dolor sit amet
+                     consectetur
+                     adipisicing elit. Voluptatibus
+                      consequuntur recusandae
+                       inventore sed minima
+                        tenetur iure corrupti.
+                         Quia aperiam odio natus?
+                          Reprehenderit accusantium
+                           deserunt voluptates recusandae
+                            fugit,
+                     praesentium doloribus
+                     Lorem ipsum dolor sit amet
+                     consectetur
+                     adipisicing elit. Voluptatibus
+                      consequuntur recusandae
+                       inventore sed minima
+                        tenetur iure corrupti.
+                         Quia aperiam odio natus?
+                          Reprehenderit accusantium
+                           deserunt voluptates recusandae
+                            fugit,
+                     praesentium doloribus
+                      aliquam!
+                </p>
+                <button className="btn btn-dark mt-5 p-3 fs-4">Download Resource</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero_2
---
+++ professional-portfolio/src/components/shared/Rate.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Rate } from 'antd';
+const App = () => <Rate />;
+export default App;
