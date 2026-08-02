41495958a0c520f8259938f65f7442235a94e8da
MohammadRabea <112767251+MohammadRabe@users.noreply.github.com>
2026-08-02 16:10:31 +0300

    Delete professional-portfolio directory

--- professional-portfolio/src/components/shared/ContactMeBtn.jsx
+++
@@ -1,7 +0,0 @@
-const ContactMeBtn = () => {
-    return (
-        <button className="btn btn-outline-light fs-2 mt-5 px-5">Contact me</button>
-    )
-}
-
-export default ContactMeBtn
--- professional-portfolio/src/components/shared/Rate.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Rate } from 'antd';
-const App = () => <Rate />;
-export default App;
